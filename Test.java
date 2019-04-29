f09ae4b06c57e1a0abccba27915c82f3330ec038
Jack <zmk1215211544@outlook.com>
2019-04-29 14:57:18 +0800

    Initial commit

---
+++ Test.java
@@ -0,0 +1,17 @@
+public class Test {
+	static final	String  id = "007";
+	static String name = "Zhang Mingkun";
+	static int age = 24;
+	public void Test(){
+
+	}
+    public static void main(String[] args) {
+
+    	float num = 43.2f;
+        System.out.println("Hello World");
+        System.out.println(1+2+3);
+        System.out.println(Test.id);
+        System.out.println(new Test().age);
+        
+    }
+}
